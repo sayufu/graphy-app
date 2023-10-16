551fb06109a30a3ccf5d12c1c5fecc2b949aa1b3
sayufu <lilifuye72@gmail.com>
2023-10-16 01:10:50 -0500

    feat: add hero section

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-  <main class="p-12 w-full h-full">
+  <nav class="sticky container mx-auto px-2 py-4">
+    <div class="flex place-content-between">
+      <img src="./assets/graphy-logo.png" alt="Graphy logo" class="h-8 md:h-12 hover:scale-95 duration-200" />
+      <div class="hidden md:flex gap-6 items-center">
+        <a id="about-link"><button class="btn-fill">Sign in</button></a>
+      </div>
+    </div>
+  </nav>
+  <main class="container mx-auto px-2 py-4">
     <router-view />
   </main>
 </template>
