21aafea14375264d0afc81f73d615ad22bcf2031
sayufu <lilifuye72@gmail.com>
2023-11-20 23:34:04 -0500

    feat: add search bar

--- src/store/pages/products.component.vue
+++ src/store/pages/products.component.vue
@@ -1,8 +1,7 @@
 <script>
-import Papa from 'papaparse';
+import { HttpsService } from "../../shared/services/https.service.js";
 import ProductCard from "../components/product-card.component.vue";
 import LoadingSpinner from "../../shared/components/loading-spinner.component.vue";
-import {HttpsService} from "../../shared/services/https.service.js";
 
 export default {
   name: 'ProductsComponent',
@@ -14,7 +13,8 @@ export default {
       products: [],
       currentPage: 1,
       itemsPerPage: 12,
-      httpService: new HttpsService()
+      httpService: new HttpsService(),
+      searchTerm: ''
     };
   },
   created() {
@@ -31,28 +31,43 @@ export default {
       return products;
     },
     calculateCurrentPageItems() {
+      const filteredProducts = this.filteredProducts();
       const startIndex = (this.currentPage - 1) * this.itemsPerPage;
       const endIndex = startIndex + this.itemsPerPage;
-      return this.products.slice(startIndex, endIndex);
+      return filteredProducts.slice(startIndex, endIndex);
     },
+    filteredProducts() {
+      const searchTerm = this.searchTerm.toLowerCase().trim();
+
+      return this.products.filter(product => {
+        const productName = (product.product || '').toLowerCase();
+        return productName.includes(searchTerm);
+      });
+    },
+    handleSearch() {
+      this.currentPage = 1; // Reset to the first page when searching
+    }
   },
 };
 </script>
 
 <template>
   <div class="container mx-auto">
-    <div v-if="products">
+    <div v-if="products.length > 0">
+      <div class="w-full my-4 px-8" >
+        <InputText v-model="searchTerm" @input="handleSearch" placeholder="Search for products" class="w-full" />
+      </div>
       <div class="flex flex-col items-center gap-8 py-8">
         <div class="flex flex-wrap justify-center -mx-4 gap-10">
           <ProductCard
-              v-for="product in calculateCurrentPageItems()"
+              v-for="product in calculateCurrentPageItems(filteredProducts())"
               :product="product"
               :key="product.id"
           />
         </div>
         <div class="flex gap-4">
-          <Button @click="currentPage -= 1" :disabled="currentPage === 1" icon="pi pi-angle-left" outlined severity="info" />
-          <Button @click="currentPage += 1" :disabled="currentPage === Math.ceil(products.length / itemsPerPage)" icon="pi pi-angle-right" outlined severity="info" />
+          <Button @click="currentPage -= 1" :disabled="currentPage === 1" icon="pi pi-angle-left" class="p-button-info p-button-outlined" />
+          <Button @click="currentPage += 1" :disabled="currentPage === Math.ceil(filteredProducts().length / itemsPerPage)" icon="pi pi-angle-right" class="p-button-info p-button-outlined" />
         </div>
       </div>
     </div>
@@ -61,5 +76,3 @@ export default {
     </div>
   </div>
 </template>
-<style scoped>
-</style>
